291303c03e4296fb4c8c2e3df5902747bf35a4c5
cguanzon <cguanzon@instructure.com>
2014-09-25 08:24:28 -0600

    continues work on compare

--- client/app/base.compare/base.compare.controller.js
+++ client/app/base.compare/base.compare.controller.js
@@ -1,6 +1,28 @@
 'use strict';
 
 angular.module('postalyzerApp')
-  .controller('BaseCompareCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('BaseCompareCtrl', function ($scope, igService) {
+        $scope.searchTerm1 = '';
+        $scope.hasSearched1 = function(){
+            if ($scope.searchTerm1.length > 0) {
+                return true;
+            }
+            return false;
+        };
+
+        $scope.user1 = {
+
+        };
+
+        $scope.searchForUser = function(searchTerm){
+            igService.searchForUser(searchTerm)
+                .then( function (res) {
+                    $scope.searchResults = res.data;
+                });
+        };
+
+        $scope.selectUser1 = function(user1){
+            $scope.user1 = user1;
+            $scope.searchTerm1 = '';
+        };
   });
